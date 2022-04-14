1c2e892fef492709f85f355749de8af29bac1c9f
Alexander Urlich <ulrichalex12@yahoo.de>
2022-04-14 17:57:52 +0200

    starter Angular App

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError,catchError} from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+const apiUrl ='https://alexandersmovieapp.herokuapp.com/';
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+export class UserRegistrationService {
+
+  constructor(private http: HttpClient) {}
+    public userRegistration(userDetails: any): Observable<any> {
+      console.log(userDetails);
+      return this.http.post(apiUrl + 'users', userDetails).pipe(
+      catchError(this.handleError)
+      );
+      }
+      private handleError(error: HttpErrorResponse): any {
+        if (error.error instanceof ErrorEvent) {
+        console.error('Some error occurred:', error.error.message);
+        } else {
+        console.error(
+            `Error Status code ${error.status}, ` +
+            `Error body is: ${error.error}`);
+        }
+        return throwError(
+        'Something bad happened; please try again later.');
+      }
+
+      getAllMovies(): Observable<any> {
+        const token = localStorage.getItem('token');
+        return this.http.get(apiUrl + 'movies', {headers: new HttpHeaders(
+          {
+            Authorization: 'Bearer ' + token,
+          })}).pipe(map(this.extractResponseData),
+          catchError(this.handleError)
+        );
+      }
+
+      private extractResponseData(res: Response): any {
+        const body = res;
+        return body || { };
+      }
+    }
