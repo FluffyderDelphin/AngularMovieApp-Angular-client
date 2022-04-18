2d27175a559594b414d068def972857dc1a4ca80
Alexander Urlich <ulrichalex12@yahoo.de>
2022-04-19 00:04:30 +0200

    implemented Login Dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,12 +19,14 @@ import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
+    UserLoginFormComponent,
   
   ],
   imports: [
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit,Input} from '@angular/core';
+import {FetchApiDataService} from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+ @Input() userData={Username:'',Password:''}
+  constructor( public fetchApiData:FetchApiDataService,
+    public dialogRef:MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+loginUser():void{
+  this.fetchApiData.loginUser(this.userData).subscribe((result:any) => {
+
+    this.dialogRef.close(); 
+    this.snackBar.open(result, 'OK', {
+       duration: 2000
+    });
+   }, (result:any) => {
+     this.snackBar.open(result, 'OK', {
+       duration: 2000
+     });
+   });
+}
+}
