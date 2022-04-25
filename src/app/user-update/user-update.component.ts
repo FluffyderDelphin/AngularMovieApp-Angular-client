ab600f697676c03f3e78848179051cd0816bd9a1
Alexander Urlich <ulrichalex12@yahoo.de>
2022-04-25 02:43:06 +0200

    added User Update Component

--- src/app/user-update/user-update.component.ts
+++ src/app/user-update/user-update.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+
+
+import {FetchApiDataService} from '../fetch-api-data.service';
+
+
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-update',
@@ -6,10 +16,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-update.component.scss']
 })
 export class UserUpdateComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() userData = {username:'',password:'',email:'',birthday:new Date()}
+  constructor(
+    public fetchApiData:FetchApiDataService,
+    public dialogRef:MatDialogRef<UserUpdateComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router) { }
+  
   ngOnInit(): void {
   }
 
+ updateUser(): void {
+    this.fetchApiData.updateUser(this.userData).subscribe((result:any) => {
+   
+     this.dialogRef.close(); 
+     this.snackBar.open(`${result.usere.username} sucessfully updated Please Login.`, 'OK', {
+        duration: 2000
+     });
+     localStorage.clear();
+     this.router.navigate(['welcome'])
+    }, (result:any) => {
+      this.snackBar.open('Update unsucsessfull', 'OK', {
+        duration: 2000
+      });
+    });
+   }
 }
