06cb7bacab2ec072e840378fbf9d3dc71b9ac73f
Alexander Urlich <ulrichalex12@yahoo.de>
2022-04-25 22:28:18 +0200

    getting fav movies

--- src/app/movie-user-favorite/movie-user-favorite.component.ts
+++ src/app/movie-user-favorite/movie-user-favorite.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-movie-user-favorite',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-user-favorite.component.scss']
 })
 export class MovieUserFavoriteComponent implements OnInit {
+  favMovies: any[]=[];
+  string:any = localStorage.getItem('user');
+  user:any = JSON.parse(this.string);
 
-  constructor() { }
+  constructor(
+    public fetchApiData:FetchApiDataService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+
+  getFavouriteMovies():void{
+    this.fetchApiData.getAllMovies().subscribe((result:any)=>{
+      this.favMovies= result.filter((m:any)=>{
+        return this.user.favMovies.includes(m._id)
+      });
+
+      return this.favMovies
+    })
+  }
 }
